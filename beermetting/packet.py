9c36566f7cf684becedd70a201c4d2a42c8de4f6
Nicolas Montoya <edward.nmontoya@gmail.com>
2020-03-22 02:00:31 -0500

    Adición de operaciones y funcionalidades a la clase paquete

--- beermetting/packet.py
+++ beermetting/packet.py
@@ -1,21 +1,27 @@
 class Packet:
 
     SERVER_REGISTER = 1
-    SEND_DATA = 2
+    SERVER_OPERATION = 2
+    SERVER_RESPONSE = 3
+    SERVER_ERROR = 4
+    SERVER_EXIT = 5
+    SEND_DATA = 6
+
+    OP_GET = 'GET'
+    OP_BROADCAST = 'GET'
 
     @staticmethod
-    def get_client_packet(msg: str):
-        packet_ob = msg.split('|')
-        if len(packet_ob) > 1:
-            return Packet(1, None, None)
+    def get_client_packet(packet_type, msg=None, usr=None):
+        return Packet(packet_type, None, None, msg, usr)
 
     @staticmethod
-    def get_server_packet(msg=None, usr=None):
-        return Packet(1, None, None, msg, usr)
+    def get_server_packet(packet_type, msg=None, usr=None):
+        return Packet(packet_type, None, None, msg, usr)
 
-    def __init__(self, packet_type, src, dst, info='', usr=None):
+    def __init__(self, packet_type, src, dst, info=None, usr=None):
         self.packet_type = packet_type
         self.src = src
         self.dst = dst
         self.info = info
         self.usr = usr
+        self.op = None
